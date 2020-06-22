1bd3123aa822147e1094a6d0b9db50398e06659a
Miguel Angel Tomairo <matomairo@gmail.com>
2020-06-22 03:23:07 -0500

    day 1

---
+++ day01/day01.playground/Contents.swift
@@ -0,0 +1,48 @@
+import UIKit
+
+var str = "Hello, playground"
+
+// Variables
+
+str = "Adios"
+
+// String y Integers
+
+var age = 36
+var population = 10_000_000
+var lastName = "Tomairo"
+
+// Multi-line strings
+
+var str2 = """
+This goes
+over mutiple
+lines
+"""
+
+// Double and Booleans
+
+var pi = 3.1415
+var awesome = true
+
+// String interpolation
+
+var score = 85
+var str3 = "You score was \(score)"
+var results = "The test results are here \(str3)"
+
+// Constants
+
+let taylor = "swift"
+
+// Type annotations
+
+let str4 = "Hola, playground"
+let album: String = "Reputation"
+let year: Int = 1983
+let height: Double = 1.70
+let taylorRocks: Bool = false
+
+// Simple types: Summary
+
+
