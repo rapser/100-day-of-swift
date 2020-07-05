54793ec97b8dff4688480bab6f41ccb97b8729f8
Miguel Angel Tomairo <matomairo@gmail.com>
2020-07-04 23:50:02 -0500

    initial day 02

---
+++ day02/day02.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+let john = "John Lennon"
+let paul = "Paul McCartney"
+let george = "George Harrison"
+let ringo = "Ringo Starr"
+
+let beatles = [john, paul, george, ringo]
+
+print(beatles[2])
+print(beatles.first!)
+print(beatles.last!)
